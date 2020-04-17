48db9b4b59c96975a8c4c6f270ded31e5d2e4f56
sasloan <sebastiansloan94@gmail.com>
2020-04-17 17:13:23 -0600

    Wrote spec for creating an endpoint

---
+++ spec/api/v1/resources/destinations/index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Destination weather', type: 'request' do 
+  it 'There should be an endpoint that has the data needed for a destinations weather show page' do
+
+    location = "denber,co"
+
+    get "/api/v1/forecast?location=#{location}"
+
+    expect(response).to be_successful
+
+    destinations = JSON.parse(response.body)
+
+    
+  end
+end
